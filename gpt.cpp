b643ff783d49e065b0eb1e24f5e5edd46f66c78b
Smruti <smruti09052004@gmail.com>
2026-01-18 11:06:26 +0530

    added gelu activation and lm_head.

---
+++ gpt.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// for feed forward block
+void mlp(){
+
+};
+
+// mha: Multi headed attention
+void mha(){
+
+};
+
+// for residual connection
+void addNorm(){
+
+};
+
+// softmax activation function
+void softmax(){
+
+};
+
+// gelu activation function
+// GELU(x)=0.5∗x∗(1+Tanh( root(2/pi)∗(x+0.044715∗x 3)))
+#define GeluScalingFactor sqrtf(2.0/M_PI)
+void gelu(float* input, float* output,int N){
+    for(int i = 0; i<N;i++){
+        float x = input[i];
+        float cube_x = x*x*x;
+        float out = 0.5*x*(1+tanhf(GeluScalingFactor*(x+0.044715*cube_x)));
+        output[i] = out;
+    }
+};
+
+// for Linear block after the attention layers
+void lm_head(
+    int vocab_size,
+    int B,
+    int T,
+    int C,
+    float* weight,
+    float* inp,
+    float* out,
+    float* bias
+) {
+    for (int b = 0; b < B; b++) {
+        for (int t = 0; t < T; t++) {
+
+            float* xid = inp + b*T*C + t*C;
+            float* yid = out + b*T*vocab_size + t*vocab_size;
+
+            for (int v = 0; v < vocab_size; v++) {
+                float* wid = weight + v*C;
+                float o = bias ? bias[v] : 0.0f;
+
+                for (int c = 0; c < C; c++) {
+                    o += xid[c] * wid[c];
+                }
+
+                yid[v] = o;
+            }
+        }
+    }
+}
+
+
+
+int main(){
+    // float* inp = new float[1];
+    // inp[0] = 1;
+    // float* out = new float[1];
+    // int N = 1;
+    // gelu(inp,out,N);
+    // for(int i = 0; i<N;i++){
+    //     cout<<inp[i]<<" "<<out[i]<<"\n";
+    // }
+
+
+    // float* inp = new float[70];
+    // float* out = new float[100];
+    // float* weight = new float[70];
+    // float* bias = nullptr;
+    // int B = 2;
+    // int T = 5;
+    // int C = 7;
+    // int V = 10;
+    // lm_head(V,B,T,C,weight,inp,out,bias);
+
+    return 0;
+}
+
+
+
+
+
+
